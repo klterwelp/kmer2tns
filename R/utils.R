b73dee58269b6a305747639be5afb17d8f68e069
klterwelp <klterwelp@gmail.com>
2024-10-28 15:00:50 -0600

    Add utility function to validate kmer count table format

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,37 @@ log_message <- function(...) {
   msg <- paste(...)
   cat(format(Sys.time(), "[%Y-%m-%d %H:%M:%S]"), msg, "\n")
 }
+
+#' Validate kmer_count_table format
+#'
+#' @param kmer_counts data.table with two columns: "kmer" and "count".
+#'
+#' @return TRUE if kmer_count is in the correct format. FALSE if not.
+#' @export
+#'
+#' @examples
+#' validate_kmer_count_table(ecoliCounts)
+validate_kmer_count_table <- function(kmer_counts) {
+  # Check if kmer_counts is a data.table
+  if (!data.table::is.data.table(kmer_counts)) {
+    stop("kmer_counts must be a data.table.")
+  }
+
+  # Check if it has exactly two columns named "kmer" and "count"
+  if (!all(c("kmer", "count") %in% colnames(kmer_counts))) {
+    stop("kmer_counts must have two columns: 'kmer' and 'count'.")
+  }
+
+  # Check if "kmer" values are all characters
+  if (!is.character(kmer_counts$kmer)) {
+    stop("The 'kmer' column must contain character values.")
+  }
+
+  # Check if "count" values are all numeric
+  if (!is.numeric(kmer_counts$count)) {
+    stop("The 'count' column must contain numeric values.")
+  }
+
+  # If all checks pass
+  return(TRUE)
+}
