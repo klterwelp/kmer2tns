adb6b7c798b286941febb88f49ce16191ad960f1
klterwelp <klterwelp@gmail.com>
2024-10-28 15:00:01 -0600

    Add ecoliCounts to data

--- R/data.R
+++ R/data.R
@@ -10,3 +10,16 @@
 #' }
 #' @source <https://www.ncbi.nlm.nih.gov/nuccore/9626372>
 "PhiXcounts"
+
+#' Ecoli k-mer counts data
+#'
+#' Counts of k-mers appearing more than once in the Ecoli genome
+#'
+#' @format
+#' A data frame with 999708 rows and 2 columns:
+#' \describe{
+#'   \item{kmer}{12-mer sequence}
+#'   \item{count}{number of times seen in the sequence}
+#' }
+#' @source <https://www.ncbi.nlm.nih.gov/nuccore/AP022815.1>
+"ecoliCounts"
---
+++ data-raw/ecoliCounts.R
@@ -0,0 +1,9 @@
+## code to prepare `ecoliCounts` dataset
+
+# Read Ecoli fasta
+ecoli_fasta <- fs::path_package("extdata", "ecoliJE86-ST05.fasta.gz", package = "kmer2tns")
+# Count k_mers of Ecoli fasta
+ecoliCounts <- count_kmers(ecoli_fasta)
+
+# Add ecoliCounts to data
+usethis::use_data(ecoliCounts, overwrite = TRUE, compress = "xz")
