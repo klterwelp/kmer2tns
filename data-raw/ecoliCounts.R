5cdfd5cb076c83580cd33f3d6d910cad602b84f8
klterwelp <klterwelp@gmail.com>
2024-11-21 13:05:43 -0700

    Add position data to ecoliCounts

--- data-raw/ecoliCounts.R
+++ data-raw/ecoliCounts.R
@@ -3,7 +3,8 @@
 # Read Ecoli fasta
 ecoli_fasta <- fs::path_package("extdata", "ecoliJE86-ST05.fasta.gz", package = "kmer2tns")
 # Count k_mers of Ecoli fasta
-ecoliCounts <- count_kmers(ecoli_fasta)
+ecoliCounts <- count_kmers_positions(ecoli_fasta)
 
 # Add ecoliCounts to data
 usethis::use_data(ecoliCounts, overwrite = TRUE, compress = "xz")
+
